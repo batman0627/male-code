adb9b3b34bd237d1fbc0c6299c0c2339eb99175f
Yaless <mfx18426417887@sina.com>
2023-09-09 18:38:38 +0800

    通用基础组件实现项目初始化

---
+++ GBC/src/main/java/com/male/GbcApplication.java
@@ -0,0 +1,15 @@
+package com.male;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mingfangxu
+ */
+@SpringBootApplication
+public class GbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbcApplication.class, args);
+	}
+}
