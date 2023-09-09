03ed721c94bceb4a1bf9b56126f804f5795933db
Yaless <mfx18426417887@sina.com>
2023-09-09 20:32:02 +0800

    logback.xml配置添加到yml

--- GBC/src/main/java/com/male/GbcApplication.java
+++ GBC/src/main/java/com/male/GbcApplication.java
@@ -2,11 +2,13 @@ package com.male;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 /**
  * @author mingfangxu
  */
 @SpringBootApplication
+@Controller
 public class GbcApplication {
 
 	public static void main(String[] args) {
