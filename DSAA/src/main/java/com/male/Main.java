1918f61aec5a35ce50e9333eec316ef8a12432bb
Yaless <mfx18426417887@sina.com>
2023-09-09 18:21:28 +0800

    数据结构算法项目初始化

---
+++ DSAA/src/main/java/com/male/Main.java
@@ -0,0 +1,17 @@
+package com.male;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mingfangxu
+ */
+@SpringBootApplication
+public class Main {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
