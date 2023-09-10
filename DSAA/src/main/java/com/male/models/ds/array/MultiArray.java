71bd539c79dfedd0631f8e191dc3641740da5e4b
Yaless <mfx18426417887@sina.com>
2023-09-10 10:23:49 +0800

    基础数据结构-数组-多维数组

---
+++ DSAA/src/main/java/com/male/models/ds/array/MultiArray.java
@@ -0,0 +1,19 @@
+package com.male.models.ds.array;
+
+/**
+ * <p>
+ * 详细描述: 多维数组
+ * </p>
+ *
+ * @author MingFangXu
+ */
+public class MultiArray {
+
+	public static void main(String[] args) {
+		int[][] array = {
+				{11, 12, 13, 14, 15},
+				{21, 22, 23, 24, 25},
+				{31, 32, 33, 34, 35},
+		};
+	}
+}
